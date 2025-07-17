6f03942ed0f70f2964a4227a96a7de21d6d8f770
meghanasvd30@gmail.com <meghanasvd30@gmail.com>
2025-07-17 04:26:24 +0000

    res

--- resource.tf
+++ resource.tf
@@ -1,19 +1,5 @@
-resource "azurerm_resource_group" "handsonterra" {
-    name     = "handsonresources"
+resource "azurerm_resource_group" "terragit" {
+    name     = "handsongit"
     location = "East US"
 
-}
- 
-resource "azurerm_storage_account" "terrahandson" {
-    name                     = "terrastoragesmegh30"
-    resource_group_name      = azurerm_resource_group.storeterra.name
-    location                 = azurerm_resource_group.storeterra.location
-    account_tier             = "Standard"
-    account_replication_type = "LRS"
-}
- 
-resource "azurerm_storage_container" "handsoncontainer" {
-    name                  = "container-handson"
-    storage_account_name  = azurerm_storage_account.saterra.name
-    container_access_type = "private"
 }
