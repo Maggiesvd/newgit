fc82aab6ddcba6d7b4aa920cc82d6a3c59357dd9
meghanasvd30@gmail.com <meghanasvd30@gmail.com>
2025-07-17 05:14:33 +0000

    res1

--- resource.tf
+++ resource.tf
@@ -1,5 +1,4 @@
-resource "azurerm_resource_group" "terragit" {
-    name     = "handsongit"
+resource "azurerm_resource_group" "terragit20" {
+    name     = "terragit20"
     location = "East US"
-
 }
